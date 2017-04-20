bde8b9aaec151423df749a925c00b13811db0ca4
tapan <tapanmca2011@gmail.com>
2017-04-20 11:55:49 +0530

    add

---
+++ src/main/java/com/spbt/rest/Application.java
@@ -0,0 +1,19 @@
+package com.spbt.rest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asderf");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
